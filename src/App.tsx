308c821a2a3debc6d58e4354ee35f510e8289224
akir0 <kirill.andreevich.dev@gmail.com>
2024-03-05 02:28:59 +0300

    feat: add loading users using axios. data from jsonplaceholder

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,23 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Card, { CardVariant } from "./components/Card";
 import UserList from "./components/UserList";
 import { IUser } from "./types/types";
+import axios from "axios";
 
 function App() {
-  const users: IUser[] = [
-    {
-      id: 1,
-      name: "Leanne Graham",
-      email: "Sincere@april.biz",
-      address: {
-        street: "Kulas Light",
-        city: "Gwenborough",
-        zipcode: "92998-3874",
-      },
-    },
-    {
-      id: 2,
-      name: "Ervin Howell",
-      email: "Shanna@melissa.tv",
-      address: {
-        street: "Victor Plains",
-        city: "Wisokyburgh",
-        zipcode: "90566-7771",
-      },
-    },
-  ];
+  const [users, setUsers] = useState<IUser[]>([]);
+
+  useEffect(() => {
+    fetchUsers()
+  }, []);
+
+  async function fetchUsers() {
+    try {
+      const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users')
+      setUsers(response.data)
+    } catch (e) {alert(e)}
+  }
   return (
     <div>
       <Card
