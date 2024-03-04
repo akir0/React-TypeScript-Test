9eede6b5f4c8ec9c914ab75fc3828e5fb108db3b
akir0 <kirill.andreevich.dev@gmail.com>
2024-03-05 01:32:00 +0300

    feat: first component. props typisation.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import Card from "./components/Card";
 
 function App() {
-  return <></>;
+  return (
+<div>
+  <Card width="200px" height="200px">
+    <button>Button</button>
+  </Card>
+</div>
+
+  )
 }
 
 export default App;
