81e02e1da3881a74c5ffaf85c79f97b82ede8173
akir0 <kirill.andreevich.dev@gmail.com>
2024-03-05 02:18:38 +0300

    feat: add UserList,IUser,IAdress,UserItem components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,44 @@
 import "./App.css";
 import Card, { CardVariant } from "./components/Card";
+import UserList from "./components/UserList";
+import { IUser } from "./types/types";
 
 function App() {
+  const users: IUser[] = [
+    {
+      id: 1,
+      name: "Leanne Graham",
+      email: "Sincere@april.biz",
+      address: {
+        street: "Kulas Light",
+        city: "Gwenborough",
+        zipcode: "92998-3874",
+      },
+    },
+    {
+      id: 2,
+      name: "Ervin Howell",
+      email: "Shanna@melissa.tv",
+      address: {
+        street: "Victor Plains",
+        city: "Wisokyburgh",
+        zipcode: "90566-7771",
+      },
+    },
+  ];
   return (
-<div>
-  <Card onClick={(num) => console.log('click', num)
-  } variant={CardVariant.outlined} width="200px" height="200px">
-    <button>Button</button>
-  </Card>
-</div>
-
-  )
+    <div>
+      <Card
+        onClick={(num) => console.log("click", num)}
+        variant={CardVariant.outlined}
+        width="200px"
+        height="200px"
+      >
+        <button>Button</button>
+      </Card>
+      <UserList users={users} />
+    </div>
+  );
 }
 
 export default App;
